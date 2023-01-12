b31e24ef74dcc6c4e538efa9526ff399c7850aa0
Sarah Glass <snglass2002@hotmail.com>
2023-01-12 11:36:18 -0600

    Finished initial workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,83 @@
+import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.ChannelSftp
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.Session
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import java.io.FileInputStream
+import java.util.*
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        gradleApi()
+        classpath("com.github.mwiede:jsch:0.2.6")
+    }
+}
+
+class ChargerPlugin : Plugin<Project> {
+    private fun establishSession() = JSch()
+        .apply { addIdentity("~/.ssh/charger") }
+        .getSession("jetson", "charger.food.is")
+        ?.apply {
+            setConfig(Properties().apply { put("StrictHostKeyChecking", "no") })
+            connect()
+        }
+
+    private fun Session.uploadZip() {
+        (openChannel("sftp") as ChannelSftp).run {
+            connect()
+            cd("/opt/charger_fuel")
+            put(FileInputStream(File("./build/libs/chargerfuel.zip")),"chargerfuel.zip")
+            disconnect()
+        }
+    }
+
+    private fun Session.runCommand(cmd: String) {
+        (openChannel("exec") as ChannelExec).run {
+            setCommand(cmd)
+            setPty(true)
+
+            val inStream = inputStream
+            val outStream = outputStream
+            setErrStream(System.err)
+            connect()
+            val read = ByteArray(1024)
+            while (true) {
+                while (inStream.available() > 0) {
+                    val i = inStream.read(read, 0, 1024)
+                    if (i < 0) break;
+                    println(String(read, 0, i))
+                }
+                if (isClosed) {
+                    println("exit-status: $exitStatus")
+                    break;
+                }
+                Thread.sleep(1000)
+            }
+            disconnect()
+        }
+    }
+
+    override fun apply(target: Project) {
+        target.task("upload") {
+            group = "charger fuel"
+//            dependsOn("clean", "zip")
+            doLast {
+                establishSession()?.run {
+                    println("Removing old files...")
+                    runCommand("rm /opt/charger_fuel/*")
+                    println("Uploading zip...")
+                    uploadZip()
+                    println("Unpacking zip...")
+                    runCommand("cd /opt/charger_fuel/ && unzip -o /opt/charger_fuel/chargerfuel.zip && rm chargerfuel.zip")
+                    disconnect()
+                }
+            }
+        }
+    }
+}
 
 plugins {
     val kotlinVersion: String by System.getProperties()
@@ -8,12 +87,13 @@ plugins {
     id("io.kvision") version kvisionVersion
 }
 
-version = "1.0.0-SNAPSHOT"
+apply<ChargerPlugin>()
+
+version = ""
 group = "com.chargerfuel"
 
 repositories {
     mavenCentral()
-    jcenter()
     mavenLocal()
 }
 
@@ -60,16 +140,4 @@ kotlin {
         implementation("io.kvision:kvision-testutils:$kvisionVersion")
     }
     sourceSets["main"].resources.srcDir(webDir)
-}
-
-val host = "charger.food.is"
-val user = "jetson"
-val pass = "uahcharger123"
-
-tasks.register("upload") {
-    group = "charger fuel"
-    dependsOn("clean","zip")
-    doLast {
-        println("//TODO: Upload to server :)")
-    }
 }
