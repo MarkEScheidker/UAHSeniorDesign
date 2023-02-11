c541c5212b7b47a2fe91060e6d2594c1d4b519fb
Sarah Glass <snglass2002@hotmail.com>
2023-02-11 17:07:03 -0600

    Backend Test

---
+++ src/backendTest/kotlin/test/com/chargerfuel/SQLTest.kt
@@ -0,0 +1,15 @@
+package test.com.chargerfuel
+
+import org.junit.Test
+
+class SQLTest {
+    @Test
+    fun fun1() {
+        //UNIT TEST 1
+    }
+
+    @Test
+    fun fun2() {
+        //UNIT TEST 2
+    }
+}
