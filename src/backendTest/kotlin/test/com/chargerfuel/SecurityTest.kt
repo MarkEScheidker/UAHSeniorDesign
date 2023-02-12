bb40fa1422f0bebd095434f4d454a2380102903e
MarkEScheidker <mark.scheidker@gmail.com>
2023-02-11 18:34:06 -0600

    Added unit tests for bcrypt and generateXByteKey, both of them pass when used under normal circumstances

--- src/backendTest/kotlin/test/com/chargerfuel/SecurityTest.kt
+++ src/backendTest/kotlin/test/com/chargerfuel/SecurityTest.kt
@@ -6,8 +6,33 @@ import org.junit.Test
 class SecurityTest {
     @Test
     fun test_generateXByteKey() {
-        val key1 = Security.generateXByteKey(1)
-        val key2 = Security.generateXByteKey(64)
+        val key1 = Security.generateXByteKey(0)
+        val key2 = Security.generateXByteKey(1)
+        val key3= Security.generateXByteKey(64)
+
+        if(key1.size != 0 || key2.size != 1 || key3.size != 64){
+            throw Exception()
+        }
+
+        if(key1.size == null || key2.size == null || key3.size == null){
+            throw Exception()
+        }
     }
 
+    @Test
+    fun test_Bcrypt() {
+        val password = "Password1!"
+        val hashed = Security.generateHashedPassword(password)
+        val matching = Security.comparePasswords(password, hashed)
+        if(matching == false) {
+            throw Exception()
+        }
+
+        val wrongPassword = "Password1!asdf"
+        val matching2 = Security.comparePasswords(wrongPassword,hashed)
+        if(matching2 == true){
+            throw Exception()
+        }
+
+    }
 }
