1496f8a89a9cbf3575ac7056e78acfb2404dc4ff
Sarah Glass <snglass2002@hotmail.com>
2023-02-16 10:51:50 -0600

    Reformatted base webpage syntax

--- src/frontendMain/kotlin/com/chargerfuel/App.kt
+++ src/frontendMain/kotlin/com/chargerfuel/App.kt
@@ -1,10 +1,12 @@
 package com.chargerfuel
 
 import com.chargerfuel.pages.*
+import com.chargerfuel.pages.Webpage.Companion.load
 import io.kvision.*
 import kotlinx.browser.window
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.*
 
 @Suppress("unused")
 val AppScope = CoroutineScope(window.asCoroutineDispatcher())
@@ -12,11 +14,12 @@ val AppScope = CoroutineScope(window.asCoroutineDispatcher())
 class App : Application() {
     override fun start(state: Map<String, Any>) {
         //Register pages
-        LoginPage.load(this)
-        MainPage.load(this)
-        SignupPage.load(this)
-        ResetPage.load(this)
-        Restaurants.load(this)
+        load(LoginPage)
+        load(MainPage)
+        load(SignupPage)
+        load(Restaurants)
+        load(ForgotPasswordPage)
+        load(ResetPasswordPage)
     }
 }
 
--- src/frontendMain/kotlin/com/chargerfuel/pages/Webpage.kt
+++ src/frontendMain/kotlin/com/chargerfuel/pages/Webpage.kt
@@ -12,18 +12,22 @@ import io.kvision.utils.vh
 import io.kvision.utils.vw
 
 abstract class Webpage(private val name: String) {
-    protected abstract val html: Root.() -> Unit
-
-    fun load(app: Application) = app.run {
-        root(name) {
-            width = 100.vw; height = 100.vh
-            div {
-                zIndex = -1
-                background = Background(Color.name(Col.DARKBLUE))
-                position = Position.FIXED
+    companion object {
+        fun Application.load(page: Webpage) {
+            root(page.name) {
                 width = 100.vw; height = 100.vh
+                div {
+                    zIndex = -1
+                    background = Background(Color.name(Col.DARKBLUE))
+                    position = Position.FIXED
+                    width = 100.vw; height = 100.vh
+                }
+                page.html(this)
             }
-            html()
         }
     }
+
+    protected abstract val html: Root.() -> Unit
+
+
 }
