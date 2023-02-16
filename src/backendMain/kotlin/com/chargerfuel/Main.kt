0206ef56c2e5342fe8c7bb6c07f257657968358d
Sarah Glass <snglass2002@hotmail.com>
2023-02-16 10:53:38 -0600

    Temporarily broke main: Will fix

--- src/backendMain/kotlin/com/chargerfuel/Main.kt
+++ src/backendMain/kotlin/com/chargerfuel/Main.kt
@@ -7,7 +7,10 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
+import io.kvision.remote.applyRoutes
+import io.kvision.remote.getServiceManager
 import io.kvision.remote.kvisionInit
+import kotlinx.coroutines.runBlocking
 
 @Suppress("unused")
 fun Application.main() {
@@ -15,11 +18,11 @@ fun Application.main() {
 
     install(Authentication) {
         form("login") {
-            userParamName = "username"
+            userParamName = "email"
             passwordParamName = "password"
 
             validate {
-                if (Security.comparePasswords(it.password,SQLUtils.getHashedPW(it.name))) {
+                if (Security.comparePasswords(it.password, SQLUtils.getHashedPW(it.name))) {
                     UserIdPrincipal(it.name)
                 } else {
                     null
@@ -45,55 +48,78 @@ fun Application.main() {
     }
 
     routing {
-        fun nonAuthRoute(nonAuth: String, fail: String) =
-            get("/$nonAuth") {
-                call.sessions.get<UserSession>()
-                    ?.let { call.respondRedirect("/$fail") }
-                    ?: call.respondHtml(nonAuth)
-            }
+        //Account Login
+        get("/") { call.respondRedirect("/login") }
+        get("/login") {
+            call.sessions.get<UserSession>()
+                ?.let { call.respondRedirect("/main") }
+                ?: call.respondHtml("login")
+        }
         authenticate("login") {
             post("/login") {
                 call.sessions.set(UserSession(call.principal<UserIdPrincipal>()?.name.toString()))
                 call.respondRedirect("/main")
             }
         }
-        authenticate("sesh") {
-            get("/main") { call.respondHtml("main") }
-            get("/logout") {
-                call.sessions.clear<UserSession>()
-                call.respondRedirect("/login")
-            }
-        }
-        get("/test") { call.respondHtml("test") }
-        nonAuthRoute("login", "main")
-        nonAuthRoute("signup", "main")
-        post("/signup") {
-            val parameters = call.receiveParameters()
-            val email = parameters["email"] ?: return@post
-            val password = parameters["password"] ?: return@post
-            //TODO check for email usage and/or send email verification
-            EmailService.sendEmailValidation(email,Security.generateSecureToken())
-            //TODO Send packet to caller to tell to check email
-        }
-        nonAuthRoute("reset", "main")
-        post("/reset"){
-            val parameters = call.receiveParameters()
-            val email = parameters["email"] ?: return@post
-            if(SQLUtils.doesUserExist(email)){
-                val pwtoken = Security.generateSecureToken()
-                TokenStorage.storePWToken(email,pwtoken)
-                EmailService.sendPasswordReset(email,pwtoken)
-                //TODO Send packet to caller to tell to check email
-                call.respondRedirect("/login")
-            } else {
-                call.respondRedirect("/reset")
-            }
+        get("/logout") {
+            call.sessions.clear<UserSession>()
+            call.respondRedirect("/login")
         }
 
-        get("/") { call.respondRedirect("/login") }
-        get("/index.html") { call.respondRedirect("/login") }
-        //TODO This spit a ton of errors in the backend log, find another way...
-//        intercept(ApplicationCallPipeline.Fallback) { call.respondRedirect("/login") }
+        //Account Creation
+        get("/signup") { call.respondHtml("signup") }
+        post("/signup") {
+            val parameters = call.receiveParameters()
+            val email = parameters["email"] ?: TODO("Tell user error has occurred and to try again")
+            val password = parameters["password"] ?: TODO("Tell user error has occurred and to try again")
+            if (!SQLUtils.isEmailRegistered(email)) {
+                val token = Security.generateSecureToken()
+                TokenStorage.setAccountCreationToken(email, token)
+                EmailService.sendEmailValidation(email, token)
+                //TODO Tell user to check email
+            } else TODO("Tell user email is already registered")
+        }
+        get("/verify") {
+            call.request.queryParameters["id"]?.let { TokenStorage.getAccountCreationToken(it) }
+                ?.let { /*TODO Verify account*/ }
+                ?: call.respondRedirect("/signup")
+        }
+
+        //Password Resetting
+        post("/forgot") {
+            val parameters = call.receiveParameters()
+            if (parameters.contains("email"))
+
+
+
+            val email = call.receiveParameters()["email"] ?: TODO("Tell user error has occurred and to try again")
+            if (SQLUtils.isEmailRegistered(email)) {
+                val token = Security.generateSecureToken()
+                TokenStorage.setPasswordRequestToken(email, token)
+                EmailService.sendPasswordReset(email, token)
+                //TODO Tell user to check email
+            } else TODO("Tell user email is not registered")
+        }
+        get("/reset") {
+            val token = call.request.queryParameters["id"]
+            token?.let { TokenStorage.getPasswordRequestToken(it) }?.let {
+                TokenStorage.setPasswordResetToken(it, token)
+                call.respondHtml("reset")
+            } ?: call.respondRedirect("/forgot")
+        }
+        post("/reset") {
+            val password = call.receiveParameters()["password"] ?: TODO("Tell user error has occurred and to try again")
+
+            SQLUtils.setHashedPW()
+        }
+
+        //Logged In Pages
+        authenticate("sesh") {
+            get("/main") { call.respondHtml("main") }
+        }
+
+        //Testing
+        get("/test") { call.respondHtml("test") }
     }
     kvisionInit()
 }
