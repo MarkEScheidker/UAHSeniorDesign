fde6f6e8398d917627ff6cee88cee93077bffa5a
Sarah Glass <snglass2002@hotmail.com>
2023-02-16 22:35:40 -0600

    Removed password reset form

--- src/commonMain/kotlin/com/chargerfuel/info.kt
+++ src/commonMain/kotlin/com/chargerfuel/info.kt
@@ -6,7 +6,4 @@ import kotlinx.serialization.Serializable
 data class LoginInfo(val email: String, val password: String)
 
 @Serializable
-data class RequestPasswordResetInfo(val email: String)
-
-@Serializable
-data class PasswordResetInfo(val password: String, val hash: String)
+data class PasswordResetInfo(val email: String)
