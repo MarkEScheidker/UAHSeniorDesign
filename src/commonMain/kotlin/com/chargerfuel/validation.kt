cd84f65267554f8f52471b0a2bed14f083fc89ad
Sarah Glass <snglass2002@hotmail.com>
2023-02-20 16:13:50 -0600

    Updated password validation

--- src/commonMain/kotlin/com/chargerfuel/validation.kt
+++ src/commonMain/kotlin/com/chargerfuel/validation.kt
@@ -3,10 +3,12 @@ package com.chargerfuel
 val passwordValidation: (String?) -> Boolean = {
     it != null &&
             it.length >= 8
+            && it.length <= 32
             && it.contains("[A-Z]".toRegex())
             && it.contains("[a-z]".toRegex())
             && it.contains("[0-9]".toRegex())
             && it.contains("[@\$!%*#?&]".toRegex())
+            && !it.contains("[^A-Za-z0-9@\$!%*#?&]".toRegex())
 }
 
 val passwordValidationMessage: (String?) -> String = {
@@ -15,6 +17,7 @@ val passwordValidationMessage: (String?) -> String = {
     else if (!it.contains("[a-z]".toRegex())) "Password must contain a lowercase letter"
     else if (!it.contains("[0-9]".toRegex())) "Password must contain a number"
     else if (!it.contains("[@\$!%*#?&]".toRegex())) "Password must contain a special character"
+    else if (it.contains("[^A-Za-z0-9@\$!%*#?&]".toRegex())) "Password contains invalid character"
     else ""
 }
 
