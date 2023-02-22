5d3df6e02a2e419b36084b86c6b05b4e45535a87
Sarah Glass <snglass2002@hotmail.com>
2023-02-21 20:00:14 -0600

    Updated toolbar with actual links

--- src/frontendMain/kotlin/com/chargerfuel/util/common.kt
+++ src/frontendMain/kotlin/com/chargerfuel/util/common.kt
@@ -1,6 +1,7 @@
 package com.chargerfuel.util
 
 import io.kvision.core.*
+import io.kvision.html.div
 import io.kvision.html.image
 import io.kvision.html.link
 import io.kvision.panel.FlexPanel
@@ -28,24 +29,40 @@ fun Container.centeredBox(init: FlexPanel.() -> Unit) {
 
 fun Container.toolbar() {
     hPanel(justify = JustifyContent.SPACEBETWEEN, alignItems = AlignItems.CENTER) {
-        width = 100.vw; height = 10.vh
+        width = 100.vw; maxWidth = 100.vw; minWidth = 100.vw
+        height = 10.vh; maxHeight = 10.vh; minHeight = 10.vh
         background = Background(Color.name(Col.ALICEBLUE))
         borderBottom = Border(5.px, BorderStyle.SOLID, Color.name(Col.MIDNIGHTBLUE))
         boxShadow = BoxShadow(0.px, 0.px, 5.px, 5.px, Color.name(Col.MIDNIGHTBLUE))
         hPanel(justify = JustifyContent.START, alignItems = AlignItems.CENTER) {
             paddingLeft = 2.vh
-            gridColumnGap = 20
-            link("Link1", "/") {
+            height = 80.perc
+            maxWidth = 80.perc
+            float = PosFloat.LEFT
+            setStyle("column-gap", "2vh")
+            link("Shop", "/test") {//TODO Change this to real restaurant page @Bailey
                 fontSize = 3.vh
                 textDecoration = TextDecoration(TextDecorationLine.NONE)
                 colorName = Col.BLACK
             }
-            link("Link2", "/") {
+            div {
+                background = Background(Color.name(Col.BLACK))
+                height = 100.perc
+                width = 2.px
+                setStyle("border-radius", "2px")
+            }
+            link("Orders", "/orders") {
                 fontSize = 3.vh
                 textDecoration = TextDecoration(TextDecorationLine.NONE)
                 colorName = Col.BLACK
             }
-            link("Link3", "/") {
+            div {
+                background = Background(Color.name(Col.BLACK))
+                height = 100.perc
+                width = 2.px
+                setStyle("border-radius", "2px")
+            }
+            link("Account", "/account") {
                 fontSize = 3.vh
                 textDecoration = TextDecoration(TextDecorationLine.NONE)
                 colorName = Col.BLACK
@@ -53,7 +70,9 @@ fun Container.toolbar() {
         }
         image(require("img/ico.png") as? String, "icon") {
             maxHeight = 8.vh
+            maxWidth = 20.perc
             paddingRight = 2.vh
+            float = PosFloat.RIGHT
             setStyle("object-fit", "contain")
         }
     }
