c489edad55620bbf170d9664c3c0f157a8067d43
Sarah Glass <snglass2002@hotmail.com>
2023-03-27 14:22:42 -0500

    Disabled menu items now work

--- src/commonMain/kotlin/com/chargerfuel/menus.kt
+++ src/commonMain/kotlin/com/chargerfuel/menus.kt
@@ -3,7 +3,7 @@ package com.chargerfuel
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Item(val name: String, val description: String, val price: Int, val disabled: Boolean = false)
+data class Item(val name: String, val description: String, val price: Int, val disabled: Boolean)
 
 @Serializable
 data class SubMenu(val name: String, val items: Map<Int, Item>)
